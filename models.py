cca33433b7e97316bff2e530531f76ed641e2953
raz-perry <perryraz38@gmail.com>
2021-06-03 19:16:00 +0300

    main driver open

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, Ridge
 from sklearn.metrics import r2_score, mean_squared_error
 
 cols = ['id', 'belongs_to_collection','budget','genres','homepage','original_title','overview','vote_average','vote_count''production_companies''production_countries','release_date','runtime','spoken_languages','status','tagline','title','keywords','cast','crew','revenue','en','es','fr','he','hi','it','other_languages']
@@ -23,17 +23,18 @@ y_val_vote_average = val_df['vote_average'].to_numpy()
 curr_val_df = val_df[['budget','vote_count','runtime','en','es','fr','he','hi','it','other_languages']]
 X_val = curr_val_df.to_numpy()
 
+def linear_reg():
 # linerar regression - revenue
-reg = LinearRegression()
-reg.fit(X, y_revenue)
-y_hat = reg.predict(X_val)
-print(X_val.size, y_val_revenue.size, y_hat.size)
+    reg = LinearRegression()
+    reg.fit(X, y_revenue)
+    y_hat = reg.predict(X_val)
+    print(X_val.size, y_val_revenue.size, y_hat.size)
 
-MSE = mean_squared_error(y_val_revenue, y_hat)
+    MSE = mean_squared_error(y_val_revenue, y_hat)
 
-# The coefficients
-print('Coefficients: \n', reg.coef_)
-# The mean squared error
-print('Mean squared error: %.2f' % MSE)
-# The coefficient of determination: 1 is perfect prediction
-print('Coefficient of determination: %.2f'% r2_score(y_val_revenue, y_hat))
+    # The coefficients
+    print('Coefficients: \n', reg.coef_)
+    # The mean squared error
+    print('Mean squared error: %.2f' % MSE)
+    # The coefficient of determination: 1 is perfect prediction
+    print('Coefficient of determination: %.2f'% r2_score(y_val_revenue, y_hat))
