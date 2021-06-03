0719a65b1ffd89dd53f1a70ac770ed243d6b2b7b
neria <neria.adelman@mail.huji.ac.il>
2021-06-03 18:14:46 +0300

    languages pre processing fix

--- categories_preprocessing.py
+++ categories_preprocessing.py
@@ -27,5 +27,27 @@ def preprocess_original_language(data):
 
     return data
 
+
+def preprocess_original_language(data):
+    # creating a row for every language with more than 30 movies:
+    LANGUAGE_THRESHOLD = 30  # amount of movies a language needs to appear in to have its own column
+
+    languages = data["original_language"].unique()
+    languages_count = data["original_language"].value_counts()
+    languages = np.column_stack((languages, languages_count))
+
+    lang_data = pd.get_dummies(data.original_language)
+    lang_data["other_languages"] = 0
+    for lang in languages:
+        if lang[1] < LANGUAGE_THRESHOLD:
+            lang_data["other_languages"] += lang_data[lang[0]]
+            lang_data = lang_data.drop(columns=[lang[0]])
+
+    data = pd.concat([data, lang_data], axis=1)
+    data = data.drop(columns=["original_language"])
+
+    return data
+
+
 def preprocess_status(data):
     return data.drop((data[data["status"] != "Released"]).index)
