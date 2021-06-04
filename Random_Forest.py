225de1fb0d73759d49eb916f529cc7a660d9afd0
neria <neria.adelman@mail.huji.ac.il>
2021-06-04 03:32:42 +0300

    added forest, lasso and ridge as classes to models.py

--- Random_Forest.py
+++ Random_Forest.py
@@ -1,18 +1,18 @@
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import r2_score, mean_squared_error
-
-
-class Forest:
-    def __init__(self, depth):
-        self.model = None
-        self.forest = RandomForestRegressor(max_depth=depth, random_state=0)
-
-    def fit(self, x, y):
-        self.model = self.forest.fit(x, y)
-
-    def predict(self, x):
-        return self.forest.predict(x)
-
-    def score(self, x, y):
-        y_predict = self.forest.predict(x)
-        return mean_squared_error(y, y_predict)
+# from sklearn.ensemble import RandomForestRegressor
+# from sklearn.metrics import r2_score, mean_squared_error
+#
+#
+# class Forest:
+#     def __init__(self, depth):
+#         self.model = None
+#         self.forest = RandomForestRegressor(max_depth=depth, random_state=0)
+#
+#     def fit(self, x, y):
+#         self.model = self.forest.fit(x, y)
+#
+#     def predict(self, x):
+#         return self.forest.predict(x)
+#
+#     def score(self, x, y):
+#         y_predict = self.forest.predict(x)
+#         return mean_squared_error(y, y_predict)
